3e2ff12d3496eca19e0dcf004fa31ec7995d0d27
sunny <sunshinechen2008@163.com>
2017-07-27 13:40:55 +0800

    commit

---
+++ src/com/s/tomcat/redissessions/LifecycleSupport.java
@@ -0,0 +1,85 @@
+package com.s.tomcat.redissessions;
+
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+import org.apache.catalina.Lifecycle;
+import org.apache.catalina.LifecycleEvent;
+import org.apache.catalina.LifecycleListener;
+
+public class LifecycleSupport {
+	
+	 /**
+     * Construct a new LifecycleSupport object associated with the specified
+     * Lifecycle component.
+     *
+     * @param lifecycle The Lifecycle component that will be the source
+     *  of events that we fire
+     */
+    public LifecycleSupport(Lifecycle lifecycle) {
+        super();
+        this.lifecycle = lifecycle;
+    }
+
+
+    // ----------------------------------------------------- Instance Variables
+
+    /**
+     * The source component for lifecycle events that we will fire.
+     */
+    private final Lifecycle lifecycle;
+
+
+    /**
+     * The list of registered LifecycleListeners for event notifications.
+     */
+    private final List<LifecycleListener> listeners = new CopyOnWriteArrayList<>();
+
+
+    // --------------------------------------------------------- Public Methods
+
+    /**
+     * Add a lifecycle event listener to this component.
+     *
+     * @param listener The listener to add
+     */
+    public void addLifecycleListener(LifecycleListener listener) {
+        listeners.add(listener);
+    }
+
+
+    /**
+     * Get the lifecycle listeners associated with this lifecycle. If this
+     * Lifecycle has no listeners registered, a zero-length array is returned.
+     */
+    public LifecycleListener[] findLifecycleListeners() {
+        return listeners.toArray(new LifecycleListener[0]);
+    }
+
+
+    /**
+     * Notify all lifecycle event listeners that a particular event has
+     * occurred for this Container.  The default implementation performs
+     * this notification synchronously using the calling thread.
+     *
+     * @param type Event type
+     * @param data Event data
+     */
+    public void fireLifecycleEvent(String type, Object data) {
+        LifecycleEvent event = new LifecycleEvent(lifecycle, type, data);
+        for (LifecycleListener listener : listeners) {
+            listener.lifecycleEvent(event);
+        }
+    }
+
+
+    /**
+     * Remove a lifecycle event listener from this component.
+     *
+     * @param listener The listener to remove
+     */
+    public void removeLifecycleListener(LifecycleListener listener) {
+        listeners.remove(listener);
+    }
+
+}
---
+++ src/com/s/tomcat/redissessions/Serializer.java
@@ -0,0 +1,14 @@
+package com.s.tomcat.redissessions;
+
+import java.io.IOException;
+
+public interface Serializer {
+	void setClassLoader(ClassLoader loader);
+
+	byte[] attributesHashFrom(RedisSession session) throws IOException;
+
+	byte[] serializeFrom(RedisSession session, SessionSerializationMetadata metadata) throws IOException;
+
+	void deserializeInto(byte[] data, RedisSession session, SessionSerializationMetadata metadata)
+			throws IOException, ClassNotFoundException;
+}
